96debb7e0ba0cfba9206081962c285fe5635e3bf
Jake <jakepucci26@gmail.com>
2022-08-24 12:49:01 -0600

    commit tree 335d9731e6a0109a518a856698d0b4c65ed8d392

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+// const { FetchError } = require("node-fetch");
+
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => renderBooks(json));
 }
 
 function renderBooks(books) {
